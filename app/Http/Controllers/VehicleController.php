aeb8b22d7b31da65c784c43d031a7cd7ff62a2c4
talenaa <talenabarbosa7@gmail.com>
2025-01-29 15:37:32 +0100

    feat: finished vehicle controller

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+
+class VehicleController extends Controller
+{
+    public function index()
+    {
+        $vehicles = Vehicle::all();
+
+        return view("vehicles", compact("vehicles"));
+    }
+}
