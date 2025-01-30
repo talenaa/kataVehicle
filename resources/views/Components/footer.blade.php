2ff0c2402344f5d4dded07050e3237bb77d69630
talenaa <talenabarbosa7@gmail.com>
2025-01-30 12:51:10 +0100

    style: added images to footer

--- resources/views/Components/footer.blade.php
+++ resources/views/Components/footer.blade.php
@@ -1,8 +1,8 @@
 <footer>
     <div class="social">
-        <a href="https://www.instagram.com/jkllvrs?igsh=ZG1sd2N2c3lmZWVk" target="_blank"><img src="/css/img/social/ig.png" alt="ig icon"></a>
-        <a href="https://github.com/talenaa" target="_blank"><img src="/css/img/social/gitHubIcon.png" alt="git icon"></a>
-        <a href="mailto:talenabarbosa7@gmail.com"><img src="/css/img/social/gmailLogo.webp" alt="logo gmail"></a>
+        <a href="https://www.instagram.com/jkllvrs?igsh=ZG1sd2N2c3lmZWVk" target="_blank"><img src="/public/img/ig.png" alt="ig icon"></a>
+        <a href="https://github.com/talenaa" target="_blank"><img src="/public/img/gitHubIcon.png" alt="git icon"></a>
+        <a href="mailto:talenabarbosa7@gmail.com"><img src="/public/img/gmailLogo.webp" alt="logo gmail"></a>
     </div>
     <p>&copy; 2025 Talena. All rights reserved.</p>
 </footer>
