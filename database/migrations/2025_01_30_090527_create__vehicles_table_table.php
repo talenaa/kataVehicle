d732e33079f03c080c19c6a170d4f82fc0377d9f
talenaa <talenabarbosa7@gmail.com>
2025-01-30 10:08:12 +0100

    feat: created vehicles table

---
+++ database/migrations/2025_01_30_090527_create__vehicles_table_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('type_id')->constrained("vehicle_types")->onDelete("cascade");
+            $table->string('license');
+            $table->bigInteger('total_expense');
+            $table->timestamps();
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
